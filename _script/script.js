b1646373d0d447b61d90c3541bb60a8978ec2212
Dennis Costa <56238317+fcdennis@users.noreply.github.com>
2020-07-30 16:37:27 -0300

    update

--- _script/script.js
+++ _script/script.js
@@ -1,3 +1,20 @@
 function mudaFoto(imagem){
     document.getElementById("imagemDoDia").src = "_imagens/" + imagem;
 } 
+
+var anime = window.document.querySelector('img#anime')
+        anime.addEventListener('click', clicar)
+        anime.addEventListener('mouseenter', entrar)
+        anime.addEventListener('mouseout', sair)
+
+        function clicar() {
+            anime = src"_imagens/naruto.jpg"
+        }
+        
+        function entrar() {
+            
+        }
+
+        function sair() {
+            
+        }
