e641776f91bc072c3bb23df4b0e4164b70e46a52
Dennis Costa <56238317+fcdennis@users.noreply.github.com>
2020-08-19 16:17:04 -0300

    fix of textarea in fale_comigo.html

--- _script/animes.js
+++ _script/animes.js
@@ -1,20 +1,20 @@
 function mudaImagemA() {
     let img = window.document.querySelector('img#anime')
-    img.src = "_animes/naruto.jpg"
+    img.src = "_images/_animes/naruto.jpg"
     let legenda = document.querySelector('figcaption#legenda')
     legenda.innerHTML = 'Naruto'
 }
 
 function mudaImagemB() {
     let img = window.document.querySelector('img#anime')
-    img.src = "_animes/bleach.jpg"
+    img.src = "_images/_animes/bleach.jpg"
     let legenda = document.querySelector('figcaption#legenda')
     legenda.innerHTML = 'Bleach'
 }
 
 function mudaImagemC() {
     let img = window.document.querySelector('img#anime')
-    img.src = "_animes/haikyuu.jpg"
+    img.src = "_images/_animes/haikyuu.jpg"
     let legenda = document.querySelector('figcaption#legenda')
     legenda.innerHTML = 'Haikyuu'
 }
--- testes.js
+++
@@ -1,6 +0,0 @@
-function printANumber(){
-    var soma = x + 5
-}
-var x = 0
-
-console.log(printANumber())
